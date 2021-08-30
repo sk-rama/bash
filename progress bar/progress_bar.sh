99993ae40d207636528440f94ff7c56328f4fa27
sk-rama <35261782+sk-rama@users.noreply.github.com>
2021-08-30 22:29:39 +0200

    Update progress_bar.sh

--- progress bar/progress_bar.sh
+++ progress bar/progress_bar.sh
@@ -1,20 +1,21 @@
 #/bin/bash
 
+# SECONDS is a bash special variable that returns the seconds since set.
 SECONDS=0
 
 function ProgressBar {
-# Process data
+    # Process data
     let _progress=(${1}*100/${2}*100)/100
     let _done=(${_progress}*4)/10
     let _left=40-$_done
-# Build progressbar string lengths
+    # Build progressbar string lengths
     _fill=$(printf "%${_done}s")
     _empty=$(printf "%${_left}s")
 
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%% "
+    # 1.2 Build progressbar strings and print the ProgressBar line
+    # 1.2.1 Output example:
+    # 1.2.1.1 Progress : [########################################] 100%
+    printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%% "
 }
 
 for (( c=1; c<=(end=500); c++ ))
